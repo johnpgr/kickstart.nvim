243b38120729db93bfc9c0d9a99831c37fd54be5
greidinger-reis <jp.greidinger@hotmail.com>
2023-12-28 17:46:12 -0300

    update options

--- lua/options.lua
+++ lua/options.lua
@@ -3,36 +3,47 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
 -- General
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.opt.relativenumber = true
+
 -- Make line numbers default
 vim.wo.number = true
-vim.wo.numberwidth = 1
+vim.wo.numberwidth = 2
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
+
 -- Enable break indent
 vim.o.breakindent = true
+
 -- Save undo history
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
+
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
+
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
+
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -43,6 +54,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     group = highlight_group,
     pattern = '*',
 })
---
--- local colorscheme = require('colorscheme')
--- vim.cmd('colorscheme ' .. colorscheme)
+
+-- display spaces and tab characters as `·`
+vim.cmd([[
+    set listchars=tab:··,trail:·,nbsp:·,space:·
+    set list
+]])
+
+-- set cursor to block on insert mode
+vim.opt.guicursor = 'n-v-c-i:block'
