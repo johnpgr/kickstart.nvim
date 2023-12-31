733ae7fa4a365b3dbbb589f7a118ea8d7b58b04c
greidinger-reis <jp.greidinger@hotmail.com>
2023-12-31 17:37:55 -0300

    +cursorword plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -776,6 +776,23 @@ local plugins = {
 		config = function()
 			require("textcase").setup({})
 		end,
+	},
+	{
+		'yamatsum/nvim-cursorline',
+		config = function()
+			require('nvim-cursorline').setup {
+				cursorline = {
+					enable = false,
+					timeout = 1000,
+					number = false,
+				},
+				cursorword = {
+					enable = true,
+					min_length = 3,
+					hl = { underline = true },
+				}
+			}
+		end
 	}
 }
 
