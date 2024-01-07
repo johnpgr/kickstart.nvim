68c6c59b42bb6785e05da535ca3591135fe0c435
greidinger-reis <jp.greidinger@hotmail.com>
2024-01-07 16:12:10 -0300

    change some lsp keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -15,13 +15,12 @@ local on_attach = function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>la', vim.lsp.buf.code_action, '[L]SP Code [A]ctions')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    nmap('gD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('<f2>', vim.lsp.buf.rename, 'LSP: Rename')
+    nmap('<leader>la', vim.lsp.buf.code_action, '[L]SP Code [A]ctions')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -81,13 +80,13 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
 
-local lspconfig = require('lspconfig')
+-- local lspconfig = require('lspconfig')
 
-lspconfig.htmx.setup {
-    filetypes = {
-        "html",
-    }
-}
+-- lspconfig.htmx.setup {
+--     filetypes = {
+--         "html",
+--     }
+-- }
 
 -- Make sure that .v files are treated as vlang
 vim.filetype.add({
