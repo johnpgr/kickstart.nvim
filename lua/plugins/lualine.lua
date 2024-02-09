f5f65d52e010fef48e52bda9a0432da10545cd94
johnpgr <jp.greidinger@hotmail.com>
2024-02-08 22:21:43 -0300

    updated lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,46 @@
+local function filename()
+    if vim.bo.filetype == "NvimTree" then
+        return "  Explorer"
+    else
+        local home = vim.fn.expand "$HOME" .. "/"
+        local fullpath = vim.fn.expand "%"
+        local escaped_home = home:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%1")
+        local result = fullpath:gsub("^" .. escaped_home, "")
+        return result
+    end
+end
+
+local function current_user()
+    return vim.fn.expand "$USER"
+end
+
+local function current_attached_lsps()
+    local active = vim.lsp.get_active_clients()
+    local result = ""
+    for _, value in ipairs(active) do
+        -- add a space between each client
+        if (result ~= "") then
+            result = result .. " "
+        end
+        result = result .. value.name
+    end
+
+    return result
+end
+
+local function current_indentation()
+    local indent_size = vim.bo.shiftwidth
+    return indent_size .. " spaces"
+end
+
+local function current_tab_mode()
+    local tab_mode = vim.bo.expandtab and "spaces" or "tabs"
+    return tab_mode
+end
+
 return {
     {
-        -- Set lualine as statusline
         'nvim-lualine/lualine.nvim',
-        -- See `:help lualine.txt`
         opts = {
             options = {
                 icons_enabled = true,
@@ -11,20 +49,10 @@ return {
             },
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { function()
-                    if vim.bo.filetype == "NvimTree" then
-                        return "  Explorer"
-                    else
-                        local home = vim.fn.expand "$HOME" .. "/"
-                        local fullpath = vim.fn.expand "%"
-                        local escaped_home = home:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%1")
-                        local result = fullpath:gsub("^" .. escaped_home, "")
-                        return result
-                    end
-                end },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
+                lualine_b = { 'branch', current_user, 'diff', 'diagnostics' },
+                lualine_c = { filename },
+                lualine_x = { current_attached_lsps },
+                lualine_y = { 'filetype', 'encoding', current_tab_mode, current_indentation },
                 lualine_z = { 'location' }
             },
         },
